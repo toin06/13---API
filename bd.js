1357b14f71d7c9d8d1f710d99abdfd02c0c229bf
toin06 <164547108+toin06@users.noreply.github.com>
2024-12-07 01:25:18 +0000

    a

--- bd.js
+++ bd.js
@@ -15,4 +15,46 @@ async function selectUsuarios() {
   return res.rows;
 }
 
-export { selectUsuarios };
+//bd.js
+async function selectUsuario(id) {
+  const client = await connect();
+  const query = "SELECT * FROM usuario WHERE id = $1";
+  const usuario = [id];
+  const res = await client.query(query, usuario);
+  client.release();
+  return res.rows;
+}
+
+async function selectUsuario(id) {
+  const client = await connect();
+  const query = "SELECT * FROM usuario WHERE id = $1";
+  const usuario = [id];
+  const res = await client.query(query, usuario);
+  client.release();
+  return res.rows;
+}
+
+async function insertUsuario(data) {
+  const client = await connect();
+  const query = "INSERT INTO usuario (nome,senha,email) VALUES ($1,$2,$3) ";
+  const usuario = [data.nome, data.senha, data.email];
+  await client.query(query, usuario);
+  client.release();
+}
+
+async function deleteUsuario(id) {
+  const client = await connect();
+  const query = "DELETE FROM usuario WHERE id = $1";
+  await client.query(query, [id]);
+  client.release();
+}
+
+async function updateUsuario(id, data) {
+  const client = await connect();
+  const query = "UPDATE usuario SET nome = $1, email = $2, senha = $3 WHERE id = $4";
+  const usuario = [data.nome, data.email, data.senha, id];
+  await client.query(query, usuario);
+  client.release();
+}
+
+export { selectUsuarios, selectUsuario, insertUsuario, deleteUsuario, updateUsuario };
